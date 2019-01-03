74b85d3e14f4ddadc06190cdb280cfa94a46305b
Cristiano Moraes <cmoraes.ti@gmail.com>
2019-01-03 21:47:33 -0200

    Primeiro componente funcional do React.

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './componentes/primeiro'
+
 const elemento = <h1>React 2</h1>
 
-ReactDOM.render(elemento , document.getElementById('root'))
+ReactDOM.render(<Primeiro /> , document.getElementById('root'))
