5b3f1169bffdb2c4b1c51c6f8c5aeebfe98e6e01
Cristiano Moraes <cmoraes.ti@gmail.com>
2019-01-11 07:54:14 -0200

    Herança entre componentes

--- react/exercicios/src/componentes/Pai.jsx
+++ react/exercicios/src/componentes/Pai.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import Filho from './Filho'
+import { childrenWithProps } from '../utils/utils'
+
+
 
 export default props => 
     <div>
         <h1>{props.nome} {props.sobrenome}</h1>
         <h2>Filhos</h2>
         <ul>
-            <li>
-                <Filho nome="Pedro" sobrenome={props.sobrenome} />
-                <Filho {...props} />
-                <Filho {...props}  nome = "Carla"/>
-            </li>
+            {childrenWithProps(props)}
         </ul>
     </div>
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 
 ReactDOM.render(
     <div>
-        <Pai nome="João" sobrenome="Cidão">
-            {/* Como passo os componentes filhos aqui */}
+        <Pai nome="João" sobrenome="Silva">
+            <Filho nome="Pedro" sobrenome="Silva" />
+            <Filho nome="Paulo" sobrenome="Silva" />
+            <Filho nome="Carla" sobrenome="Silva" />
         </Pai>
 
     </div>
