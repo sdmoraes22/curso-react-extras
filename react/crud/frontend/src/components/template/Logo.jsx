28dc0c7eeeab3c2d10453fbead620f7f5fa3cb07
Cristiano Moraes <cmoraes.ti@gmail.com>
2019-01-16 22:37:11 -0200

    Trabalhando no componente header

--- react/crud/frontend/src/components/template/Logo.jsx
+++ react/crud/frontend/src/components/template/Logo.jsx
@@ -1,7 +1,11 @@
 import './Logo.css'
+import logo from '../../assets/images/logo.png'
 import React from 'react'
 
+
 export default props =>
-    <aside>
-        Logo
+    <aside className="logo">
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
